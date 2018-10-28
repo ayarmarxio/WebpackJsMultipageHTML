fd585c61065f634b179c88e155a68cee0843857b
ayarmarxio <ayarmarxio@gmail.com>
2018-10-28 23:41:08 +0100

    f

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,19 @@
-import '../css/main.scss';
-import { RandomGenerator } from './random-generator';
+import "../css/main.scss";
+import "../main.html";
+import { RandomGenerator } from "./random-generator";
 
-const outputParagraph = document.querySelector('#outputParagraph');
+const outputParagraph = document.querySelector("#outputParagraph");
 
 const outputRandomInt = () => {
-    outputParagraph.textContent = RandomGenerator.randomInteger();
+  outputParagraph.textContent = RandomGenerator.randomInteger();
 };
 
 const outputRandomRange = () => {
-    outputParagraph.textContent = RandomGenerator.randomRange(1, 500);
+  outputParagraph.textContent = RandomGenerator.randomRange(1, 500);
 };
 
-const buttonRndInt = document.querySelector('#randomInt');
-const buttonRndRange = document.querySelector('#randomRange');
+const buttonRndInt = document.querySelector("#randomInt");
+const buttonRndRange = document.querySelector("#randomRange");
 
-buttonRndInt.addEventListener('click', outputRandomInt);
-buttonRndRange.addEventListener('click', outputRandomRange);
+buttonRndInt.addEventListener("click", outputRandomInt);
+buttonRndRange.addEventListener("click", outputRandomRange);
---
+++ src/js/main.ts
@@ -0,0 +1,1 @@
+console.log("Cargue por la reputa!!");
